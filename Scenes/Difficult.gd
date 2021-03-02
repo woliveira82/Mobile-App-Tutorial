c7d5f79bf2b76ad4473f9e7c49182cbd82e08880
Weiner Oliveira <woliveira82@gmail.com>
2021-03-02 00:25:00 -0300

    Expanding The Menu

---
+++ Scenes/Difficult.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func move(target):
+	var move_tween = get_node("move_tween")
+	move_tween.interpolate_property(self, "position", position, target, 2, Tween.TRANS_QUINT, Tween.EASE_OUT)
+	move_tween.start()
---
+++ Scenes/StartMenu.gd
@@ -0,0 +1,19 @@
+extends VBoxContainer
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func move(target):
+	var move_tween = get_node("move_tween")
+	move_tween.interpolate_property(self, "position", position, target, 2, Tween.TRANS_QUINT, Tween.EASE_OUT)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
